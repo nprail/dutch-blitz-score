2101654c4b2ec69fc2961cf7e6c66208483ccbd5
Noah Prail <nprail@event1.io>
2024-02-19 09:20:23 -0500

    ci: Fix package-lock.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.noahprail.blitzscore',
   appName: 'Blitz Score',
   webDir: 'build',
+  android: {
+    buildOptions: {
+      keystoreAlias: 'blitz',
+      keystorePassword: process.env.BLITZ_APP_KEYSTORE_PASS,
+      keystoreAliasPassword: process.env.BLITZ_APP_KEYSTORE_PASS,
+      keystorePath: process.env.BLITZ_APP_KEYSTORE_PATH,
+    },
+  },
 }
 
 export default config
